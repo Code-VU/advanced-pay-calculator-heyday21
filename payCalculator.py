5e0370d160675d2e7b0e88287bb1f5c615c3d4a0
Dana Schmidt <Dana.Schmidt@vu.com>
2022-01-26 08:34:06 -0600

    first attempt

--- payCalculator.py
+++ payCalculator.py
@@ -1,12 +1,20 @@
 def calculatePay():
     
     # This first line is provided for you
-    hrs = input("Enter Hours:")
+    hrs = float(input("Enter Hours: "))
+    rate = float (input("Enter Rate: ")) 
+
+    if hrs <= 40:
+        pay = hrs * rate
+        
+    else:
+        pay = (40 * rate) + ((hrs-40) *(rate*1.5))
     
+    print ("Pay: ", pay)
     # end assignment
 
 ## if you want to test locally before you try to sync
 ## uncomment calculatePay() and run > python payCalculator.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-# calculatePay()
+#calculatePay()
