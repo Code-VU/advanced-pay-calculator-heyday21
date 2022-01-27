198fe32001c32d6d2ad93fd2149f1163a7422c78
Dana Schmidt <Dana.Schmidt@vu.com>
2022-01-27 09:39:43 -0600

    removed lines

--- codevuAssignment3.1.py
+++ codevuAssignment3.1.py
@@ -1,9 +1,7 @@
 def calculatePay():
-    
     # This first line is provided for you
     hrs = float(input("Enter Hours: "))
     rate = float(input("Enter Rate: ")) 
-
     if hrs > 40:
         pay = (40*rate) + ((hrs-40)*(rate*1.5))
     else:
